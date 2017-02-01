eb6c159f2c33f58f791ddd6e9e5c574f1101cfe0
Ben Swanson <benswanson127@gmail.com>
2017-02-01 16:34:36 -0700

    Updated Tester

--- src/Tester.java
+++ src/Tester.java
@@ -10,9 +10,10 @@ public class Tester {
 		System.out.println("The basic resistor color codes are: black: 0, brown: 1, red: 2, orange: 3, yellow: 4, green: 5, blue: 6, violet: 7, gray: 8, white: 9");
 		System.out.println("Each resistor will have at least three bands, with the first and second bands indicating the first two digits of the ohms value, and the third indicating the power of ten to multiply them by, for example a resistor with the three bands 'yellow violet black' would be 47 * 10^0 ohms, or 47 ohms.");
 		System.out.println("Most resistors will also have a fourth band that is either gold or silver, with gold indicating plus or minus 5% tolerance, and silver indicating 10% tolerance. Resistors that do not have a fourth band are rated at 20% tolerance. NOTE: Encoding only works with gold band resistors");
+		System.out.println("NOTE: This program will only handle resistors less than 999Million Ohms");
+		System.out.println("");
 		Scanner input = new Scanner(System.in);
 		System.out.println("Would you like to 'Encode' or 'Decode', please type your answer or type 'exit'");
-		System.out.println("NOTE: This program will only handle resistors less than 999Million Ohms");
 		String inputString = input.next();
 		
 		while (keepGoing) {
@@ -32,9 +33,6 @@ public class Tester {
 			keepGoing = false;
 			break;
 		}
-		else {
-			System.out.println("Invalid Entry");
-			}
 		}
 	}
 }
